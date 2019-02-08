cd1ee52f4e2c92c769632911a604289c402a8f32
Anthony Chao <anthony.chao@blackrock.com>
2019-02-07 18:49:31 -0800

    Adding robot_name

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,6 +1,18 @@
-from random import randint
+from random import randint, choice
+from string import ascii_uppercase
+
 
 class Robot(object):
     def __init__(self):
-        self.name = 'AA123'
+        name = []
+        for _ in range(2):
+            name.append(choice(ascii_uppercase))
+        for _ in range(3):
+            name.append(str(randint(0, 9)))
+        self.name = ''.join(name)
+
+    def reset(self):
+        self.name = 'AB123'
+
+
 
